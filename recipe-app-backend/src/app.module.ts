1c2834a08061aeb0fff56baf67280fc8c6cbfaed
frxdric <fredricwood@gmail.com>
2024-08-03 14:41:30 -0400

    backend / frontend deployment

--- recipe-app-backend/src/app.module.ts
+++ recipe-app-backend/src/app.module.ts
@@ -4,9 +4,18 @@ import { AppService } from './app.service';
 import { RecipeModule } from './recipe/recipe.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [RecipeModule, PrismaModule, UsersModule],
+  imports: [
+    RecipeModule, 
+    PrismaModule, 
+    UsersModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
