efa3d3e980924eff393a2eda6f7b52f6b19ac3a2
frxdric <fredricwood@gmail.com>
2024-08-08 19:49:50 -0400

    troubleshooting

--- recipe-app-backend/client/dist/recipe/dto/create-recipe.dto.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class CreateRecipeDto {
-    ingredient: string;
-    quantity: number;
-}
--- recipe-app-backend/client/dist/recipe/entities/recipe.entity.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Recipe = void 0;
-class Recipe {
-}
-exports.Recipe = Recipe;
-//# sourceMappingURL=recipe.entity.js.map
